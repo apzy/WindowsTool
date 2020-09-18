540c86fb53d25ba64a3c6c5b9a4fd9bd2e700ab3
zhouyang <zyzhouyang1@outlook.com>
2020-09-18 08:06:16 +0800

    sample

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,14 +14,40 @@ using System.Windows.Shapes;
 
 namespace DesktopTool
 {
-    /// <summary>
-    /// MainWindow.xaml 的交互逻辑
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
+        /// <summary>
+        /// MainWindow.xaml 的交互逻辑
+        /// </summary>
+        public partial class MainWindow : Window
         {
-            InitializeComponent();
+                public MainWindow()
+                {
+                        InitializeComponent();
+                }
+
+
+                private void AddItem(object sender, RoutedEventArgs e)
+                {
+                        CheckBox check = new CheckBox();
+                        TextBlock text = new TextBlock();
+                        text.Text = Message.Text;
+                        text.MaxWidth = 200;
+                        text.TextWrapping = TextWrapping.Wrap;
+                        text.Foreground = new SolidColorBrush(Colors.White);
+                        check.Content = text;
+                        List.Children.Add(check);
+                }
+
+                private void WindowMouseMove(object sender, MouseEventArgs e)
+                {
+                        if (e.LeftButton == MouseButtonState.Pressed)
+                        {
+                                this.DragMove();
+                        }
+                }
+
+                private void Close(object sender, RoutedEventArgs e)
+                {
+                        this.Close();
+                }
         }
-    }
 }
